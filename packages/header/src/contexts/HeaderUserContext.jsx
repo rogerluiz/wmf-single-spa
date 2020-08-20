83c6a30c0e3020dad2c56c591ca1c0464218d711
Roger Luiz <rogerluizm@gmail.com>
2020-08-20 14:01:25 -0300

    adicionando imagens e about

---
+++ packages/header/src/contexts/HeaderUserContext.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const HeaderUserContext = React.createContext({
+  user: 'none',
+});
+
--- packages/web/src/components/About.jsx
+++ packages/web/src/components/About.jsx
@@ -57,26 +57,82 @@ function About() {
   `;
   return (
     <Containet>
-      <h1>Single SPA e Webpack Module Federation</h1>
+      <h1>Micro Front-end</h1>
 
-      <p>Webpack 5 Module Federation mira a resolver o problema de compartinhamento de modules em um sistema de distribuição, criando pedaços de código e compartinhando entra as aplicações macro ou micro, como você quiser.</p>
+      <h2>Problema</h2>
+      <p>A maioria dos projetos na VR são monólitos grandes onde várias regras de negócio se misturam fazendo com que novas funcionalidades e correções de bugs demorem muito para acontecerem, conflitos, deploys demorados e vários outros problemas são encontrados em um projeto desse tamanho.</p>
 
-      <h2>Como funciona</h2>
-      
-      <p>O Webpack Module Federation retira parte do código do pipeline separando esse código da aplicação, fazendo assim com que o componente ou componentes adicionando na lista de <i>exposes</i> sejam adicionando em um <i>remoteEntry</i></p>
+      <h2>SOLUÇÃO</h2>
+      <p>Foi criar pequenos projetos de forma que a atualização e deploy ficassem mais rápidos de serem feitos, garantindo uma dependência dos times para corrigir problemas ou criar novas funcionalidades sem impactar outros times dessa forma nasceu o micro front-end na VR.</p>
 
-      <p>Para conseguir isso, existem dois conceitos principais para se familiarizar: <b>host</b> e <b>remote</b></p>
+      <p>Após separar em diversas aplicações. Começam a surgir problemas de compatibilidade, problemas para gerenciar dependências, UIs desatualizadas criando um esforço grande para manter todas as aplicações com a mesma aparência UI.</p>
 
-      <h3>HOST</h3>
-      <p>Uma aplicatição host contém todos os recursos típicos de uma aplicação SPA (Single Page Application) ou SSR (Server Side Rendering). Carrega todos os chunks inicial, inicializa o aplicatição e renderiza o que o usuário verá primeiro. Ao invez de usar uma lib para compartinhar esse código entre aplicações você </p>
+      <p><i>Segue um exemplo da estrutura atual</i></p>
+      <img src="/single-spa-lib.png" width="100%" />
+      <br />
+      <p>Na imagem acima podemos ver que cada projeto pode ter uma versão diferente das dependências tanto da lib como do header e do footer.</p>
 
+      <br />
+      <br />
+
+      <h1>SPA (Single Page Application)</h1>
+      <br />
+      <h3>O QUE É?</h3>
+      <p>É uma aplicação web ou site que consiste de uma única página web com o objetivo de fornecer uma experiência do usuário similar à de um aplicativo desktop.</p>
+      <br />
+      <h3>FRAMEWORKS</h3>
+      <img src="/frameworks.png" alt="" />
+      <br />
+      <br />
+      <h1>SINGLE SPA / ARQUITETURA MICRO FRONT-END</h1>
+      <p>Como expliquei acima problemas de dependências e versões dessas dependências começaram a ser notadas, As dependência que criaram mais problemas foram o Header e o Footer como essas dependências fazem parte de todos os projetos acaba que cria um problema de versão maior que outras dependência.</p>
+
+      <p>O principal problema é que para cada vez que uma nova funcionalidade é criado ou um bug é resolvido todos os projetos têm que ser atualizados. Imagina que temos 10 times dentro da VR cada um trabalhando em seus projetos cada vez que o Header é atualizados todos os projetos desses times tem que ser atualizados.</p>
+
+      <p>Digamos que temos 20 ou 30 projetos micro front-end cada um desses projetos tem que ser atualizados porque o header foi atualizado, isso causa um problema grande para os times ou para um time que tem que atualizar 20 ou 30 projetos.</p>
+
+
+      <h3>COMO RESOLVER ISSO?</h3>
+      <p>Fazendo com que o header e o footer sejam compartilhados de forma com que ao invés que atualizar 20 ou 30 projetos somente o projeto afetado seja atualizado.</p>
+
+      <p><i>Aqui segue um exemplo de como essa estrutura pode ser criada</i></p>
+      <br />
+      <img src="/single-spa-estrutura.png" width="300" />
+      <br />
+      <br />
+     
+      <h1>Webpack Module Federation</h1>
+
+      <p>Webpack 5 Module Federation mira resolver problemas de compartilhamento de módulos em um sistema de distribuição, criando pedaços de código e compartilhando entre as aplicações.</p>
+
+      <h3>CONCEITO</h3>
+      <p>São distinguidos entre dois conceitos módulos locais e remotos.</p>
+      <br />
+
+      <h3>LOCAL</h3>
+      <p>Módulos locais são módulos normais que fazem parte do build atual.</p>
+      <br />
       <h3>REMOTE</h3>
-      
-      <p>Abaixo segue um exemplo da configuração</p>
-      
+      <p>Módulos remotos são módulos que não fazem parte do build atual e são carregados de um contêiner em tempo de execução.</p>
+      <p>O carregamento de módulos remotos é considerado uma operação assíncrona. Ao usar um módulo remoto, essas operações assíncronas serão colocadas na próxima operação de carregamento do chunk que está entre o módulo remoto e o ponto de entrada.</p>
+      <br />
+      <h3>MODULOS COMPARTILHADOS</h3>
+      <p>Módulos compartilhados são módulos que podem ser substituídos e fornecidos como substituições ao contêiner local. Eles geralmente apontam para o mesmo módulo em cada build, por exemplo, a mesma biblioteca.</p>
+
+      <br />
+      <p><i>Exemplo da configuração</i></p>
       <Code>
         <Pre dangerouslySetInnerHTML={{__html: Prism.highlight(code, Prism.languages.javascript, 'javascript') }} />
       </Code>
+      x
+      <br />
+      <script src="https://gist.github.com/rogerluiz/8348b17e7a4b8497d1b55806f205d7b7.js" async />
+      {/* <div dangerouslySetInnerHTML={{__html: `<script src="https://gist.github.com/rogerluiz/8348b17e7a4b8497d1b55806f205d7b7.js"></script>` }} /> */}
+      {/* <h3>Estrutura</h3>
+      <p>Usando o Module Federation conseguiriamos fazer com que nossas dependencias como header e footer entre outras sejam controlados e atualizados em todos os projetos de forma rapida e evitando os problemas citados no inicio</p>
+      <br />
+      <img src="/single-spa-container.png" width="500" />
+      <br /> */}
     </Containet>
   )
 }
